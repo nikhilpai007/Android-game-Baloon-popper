5fcf618e6b9dc5c09426851144aa5b55420150e6
Gokhan Karasu <gokhankarasu@trentu.ca>
2018-04-02 18:01:43 -0400

    main

--- app/src/main/java/com/example/nik/popthebaloon/MainActivity.java
+++ app/src/main/java/com/example/nik/popthebaloon/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getWindow().setBackgroundDrawableResource(R.drawable.images);
         mContentView = findViewById(R.id.activity_main);
     }
 
