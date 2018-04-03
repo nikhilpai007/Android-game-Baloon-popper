03c115ae2a3bd2e7ba1ffe905fa45e1eedb33b19
nikhilpai007 <nikhilpaiganesh@trentu.ca>
2018-04-03 19:52:14 -0400

    JAVA for Balloons popping up

--- app/src/main/java/com/example/nik/popthebaloon/Baloon.java
+++ app/src/main/java/com/example/nik/popthebaloon/Baloon.java
@@ -1,7 +1,10 @@
 package com.example.nik.popthebaloon;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 import com.example.nik.popthebaloon.utility.PixelHelper;
@@ -10,7 +13,9 @@ import com.example.nik.popthebaloon.utility.PixelHelper;
  * Created by Nik on 03/04/18.
  */
 
-public class Baloon extends android.support.v7.widget.AppCompatImageView {
+public class Baloon extends android.support.v7.widget.AppCompatImageView implements ValueAnimator.AnimatorUpdateListener, Animator.AnimatorListener {
+
+    private ValueAnimator mAnimator;
 
     public Baloon(Context context) {
         super( context );
@@ -18,7 +23,7 @@ public class Baloon extends android.support.v7.widget.AppCompatImageView {
          public Baloon(Context context, int color, int rawHeight) {
             super( context );
 
-            this.setImageResource( R.drawable.baloon);
+            this.setImageResource( R.drawable.balloon);
             this.setColorFilter(color);
 
             int rawWidth = rawHeight/2;
@@ -26,5 +31,44 @@ public class Baloon extends android.support.v7.widget.AppCompatImageView {
             int dpHeight = PixelHelper.pixelsToDp( rawHeight, context );
             int dpWidth = PixelHelper.pixelsToDp( rawWidth, context );
              ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(dpWidth, dpHeight );
+             setLayoutParams( params );
+    }
+
+    public void baloongo(int heightscrn, int time ) {
+        mAnimator = new ValueAnimator();
+        mAnimator.setDuration( time );
+        mAnimator.setFloatValues( heightscrn, 0f);
+        mAnimator.setInterpolator(new LinearInterpolator(  ) );
+        mAnimator.setTarget(this);
+        mAnimator.addListener(this);
+        mAnimator.addUpdateListener(this);
+        mAnimator.start();
+
+    }
+
+    @Override
+    public void onAnimationStart(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animator) {
+
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        setY( (float) valueAnimator.getAnimatedValue() );
+
     }
 }
